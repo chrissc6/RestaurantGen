608820c82497800c88408e57505424965b99b677
chris <christopherscaudill@gmail.com>
2019-02-23 18:50:29 -0500

    ic

--- RestaurantGen/Form1.Designer.cs
+++ RestaurantGen/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(258, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restaurant Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(638, 69);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Choice Is..";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "RanGen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Grab & Go",
+            "Pizza",
+            "Dine Out",
+            "Any & All"});
+            this.comboBox1.Location = new System.Drawing.Point(298, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Any & All";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Edit Choice:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ResGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- RestaurantGen/Form1.cs
+++ RestaurantGen/Form1.cs
@@ -12,9 +12,157 @@ namespace RestaurantGen
 {
     public partial class Form1 : Form
     {
+        string[] Gg = new string[] { "McDonalds", "Five Guys", "Qdoba", "Captain Ds", "Gold Star", "Piada", "Arbys", "Currito", "Subway", "Potbelly Sandwich", "Penn Station" };
+        string[] Pz = new string[] { "LaRosas", "Donatos", "Deweys", "Dominos", "Pizza Hut" };
+        string[] Dn = new string[] { "PF Changs", "Rusty Bucket", "Buffalo Wild Wings", "Bonefish Grill", "J Alexanders", "Bravo", "Seasons 52", "Boston Market" };
+
+        bool GgOnly = false;
+        bool PzOnly = false;
+        bool DnOnly = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Random rng = new Random();
+            int g;
+            g = rng.Next(0, 3);
+            int x;
+            string[] p;
+
+            g = CheckSelect(g);
+            x = GetName1(g);
+            p = GetName2(g);
+
+            int y;
+            y = rng.Next(0, x);
+
+            string c = p[y];
+
+            label2.Text = c;
+        }
+
+        private int CheckSelect(int g)
+        {
+            int z = 0;
+
+            if(GgOnly == true)
+            {
+                z = 0;
+                return z;
+            }
+            else if(PzOnly == true)
+            {
+                z = 1;
+                return z;
+            }
+            else if(DnOnly == true)
+            {
+                z = 2;
+                return z;
+            }
+            else
+            {
+                z = g;
+                return z;
+            }
+        }
+
+        private string[] GetName2(int x)
+        {
+            string[] c;
+
+            switch (x)
+            {
+                case 0:
+                    c = Gg;
+                    return c;
+                case 1:
+                    c = Pz;
+                    return c;
+                case 2:
+                    c = Dn;
+                    return c;
+                default:
+                    c = null;
+                    return c;
+
+            }
+        }
+
+        private int GetName1(int x)
+        {
+
+            switch (x)
+            {
+                case 0:
+                    x = Gg.Length;
+                    return x;
+                case 1:
+                    x = Pz.Length;
+                    return x;
+                case 2:
+                    x = Dn.Length;
+                    return x;
+                default:
+                    x = 0;
+                    return x;
+                    
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(GgOnly == true)
+            {
+                GgOnly = false;
+            }
+            else
+            {
+                GgOnly = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (PzOnly == true)
+            {
+                PzOnly = false;
+            }
+            else
+            {
+                PzOnly = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GgOnly = false;
+            PzOnly = false;
+            DnOnly = false;
+
+            if (comboBox1.Text == "Grab & Go")
+            {
+                GgOnly = true;
+            }
+            else if(comboBox1.Text == "Pizza")
+            {
+                PzOnly = true;
+            }
+            else if(comboBox1.Text == "Dine Out")
+            {
+                DnOnly = true;
+            }
+            else if(comboBox1.Text == "Any & All")
+            {
+                GgOnly = false;
+                PzOnly = false;
+                DnOnly = false;
+            }
         }
     }
 }
